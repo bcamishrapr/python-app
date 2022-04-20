960ac97ff4556a3a05c7f6e691fc11131be6d3bf
Prasoon Mishra <43287406+bcamishrapr@users.noreply.github.com>
2022-04-20 12:13:21 +0530

    Update hello.py

--- src/hello.py
+++ src/hello.py
@@ -5,4 +5,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'This is sample application running on Flask! ADDING KUBERNETES'
+    #return 'This is sample application running on Flask! ADDING KUBERNETES'
+    return render_template('index.html')
